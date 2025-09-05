28fa88178abcc8799ad7b676567a73a3d48ef07b
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2025-09-05 13:20:10 -0300

    ambiente configurado... iniciando

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import './App.css'
+import "./components/Header"
+import { Header } from "./components/Header";
+
+interface Tarefa{
+  id:number;
+  texto:string;
+  concluida:boolean;
+}
 function App() {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+  return(
+   <Header/>
+   
+  )
 }
 
 export default App
---
+++ to-do-list/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div>
+      <h1  className="text-4xl font-bold text-center" >TO DO LIST</h1>
+    </div>
+  )
+}
